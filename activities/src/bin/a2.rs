315bebe3f5dde42b1c2f6f809c6074a15a6e2708
tail-s <lunar7s@naver.com>
2024-09-10 23:51:22 +0900

    .

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -4,8 +4,19 @@
 // * Displays the result of the sum of two numbers
 //
 // Notes:
-// * Use a function to add two numbers together
-// * Use a function to display the result
-// * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+// * Use a function to add two numbers together
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// * Use a function to display the result
+fn display_result(result: i32) {
+    // * Use the "{:?}" token in the println macro to display the result
+    println!("{:?}", result);
+}
+
+fn main() {
+    let result = sum(2, 3);
+    display_result(result);
+}
