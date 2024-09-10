d8dafb41faa87ae21ae7a20b468abf7cfc075405
tail-s <lunar7s@naver.com>
2024-09-11 00:21:45 +0900

    .

--- activities/src/bin/a3a.rs
+++ activities/src/bin/a3a.rs
@@ -10,4 +10,15 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    // * Displays a message based on the value of a boolean variable
+    let my_bool = true;
+    // * When the variable is set to true, display "hello"
+    if my_bool {
+        println!("hello");
+    } else {
+        println!("goodbye");
+    }
+    // * When the variable is set to false, display "goodbye"
+
+}
